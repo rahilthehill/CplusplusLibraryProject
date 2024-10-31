2451d8bffd6fe87709aa102876c8e95910a97466
rahilthehill <69742190+rahilthehill@users.noreply.github.com>
2024-10-31 05:34:59 -0400

    Made program more robust Inputting books its now a function rather than everything the user is given choice on what to do next try and catch exceptions invariants & asserts way to progressively add more books too (hence the move to a external function for adding books)

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,9 @@
 #include "Book.hpp"
 #include <iostream>
+#include <fstream>
+#include <cassert>
+#include <stdexcept>
+
 using namespace std;
 
 void output(const string &title, const string &author,const int &year) {
@@ -10,6 +14,10 @@ void output(const string &title, const string &author,const int &year) {
 }
 
 void sortBooksByYear(Book books[], int size) {
+    if (size < 2) {
+        cout << "Can't sort 1 book!" << endl;
+        cout << "Try adding more!" << endl;
+    }
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (books[j].year_published > books[j + 1].year_published) {
@@ -19,5 +27,83 @@ void sortBooksByYear(Book books[], int size) {
                 books[j + 1] = temp;
             }
         }
+        cout << "Sorted!" << endl;
     }
+}
+
+void exportImport(Book books[], int size) {
+    fstream file;
+    file.open("Library.txt", ios::out);
+    if (file.is_open()) {
+        file << "Library list" << endl;
+        file << "_____________" << endl;
+        for (int i = 0; i < size; i++) {
+            file << "Title of book: " << books[i].title << endl;
+            file << "Author of book: " << books[i].author << endl;
+            file << "Year published: "<< books[i].year_published << endl;
+            file << "*-*-*-*-*-*-*-*-*-*" << endl;
+        }
+        file.close();
+    }
+
+    file.open("Library.txt", ios::in);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    }
+}
+
+int calculateAverage(Book books[], int size) {
+
+    //assertions added here to validate parameters
+    // assert(books != nullptr);
+    // assert(size > 0);
+
+    //inceptions and variants
+    if (size <= 0) {
+        throw invalid_argument("CAN'T HAVE A EMPTY LIBRARY");
+    }
+    if (books == nullptr) {
+        throw invalid_argument("Books list is empty");
+    }
+
+    int temp{0};
+    for(int i = 0; i < size; i++) {
+        temp = books[i].year_published + temp;
+    }
+
+    return temp/size;
+}
+
+void addBook(vector<Book>& books, int& bookAmt, const string& name) {
+    int booksToAdd;
+
+    cout << "How many books would you like to add to " << name << "? (Minimum 1)" << endl;
+    cin >> booksToAdd;
+    cin.ignore();
+
+    bookAmt += booksToAdd;
+
+    int currentSize = books.size();
+    books.resize(currentSize + booksToAdd);
+
+    for (int i = currentSize; i < currentSize + booksToAdd; i++) {
+        cout << "Enter details for book " << (i - currentSize + 1) << endl;
+        cout << "Title: " << endl;
+
+        getline(cin, books[i].title);
+        cout << "Author of book:"<< endl;
+
+        getline(cin, books[i].author);
+        cout << "Year published: " << endl;
+        cin >> books[i].year_published;
+        cin.ignore(); // clears bugger before using getline
+    };
+
+
+    cout << "Thanks for adding your books!" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,43 +1,104 @@
 #include <iostream>
 #include "Book.hpp"
 #include <vector>
+#include <cctype>
 
 using namespace std;
 
 int main() {
+    int bookAmt{0};
+    string libName{"User's Library"};
 
-    int bookAmt{1};
     cout << "***************************************************" << endl;
     cout << "Welcome to your custom library software" << endl;
     cout << "***************************************************" << endl;
-    cout << "How many books would you like to add? (Default 1)" << endl;
-    cin >> bookAmt;
-    cin.ignore();
+    cout << "Name your new library!: " << endl;
+    getline(cin, libName);
+
 
     //Book books[bookAmt];
     //this is bad cause c++ base doesnt support dynamic assisngment like this, using a vector instead
 
     vector<Book> books(bookAmt);
+    addBook(books, bookAmt,libName);
 
-    for (int i = 0; i < bookAmt; i++) {
-        cout << "Enter details for book " << i+1 << endl;
-        cout << "Title: " << endl;
-        cin.ignore(); // clears bugger before using getline
-        getline(cin, books[i].title);
-        cout << "Author of book:"<< endl;
-        getline(cin, books[i].author);
-        cout << "Year published: " << endl;
-        cin >> books[i].year_published;
 
-    };
+    int choice{0};
+    while (choice != 10) {
+        cout << "What would you like to do next?" << endl;
+        cout << "Your choice (1-10): " << endl;
+        cout << "1 - Sort by published year" << endl << "2 - Show sorted List" << endl << "3 - Calculate average year published?" << endl;
+        cout << "4 - Export To External File" << endl << "5 - Add more books to " << libName << endl << "6 - Emulate a try and catch exception! [for the dev :)]" << endl;
+        cout << "7 - WIP" << endl << "8 - WIP" << endl << "9 - WIP" << endl << "10 - Exit"<< endl;
+        cin >> choice;
 
-    sortBooksByYear(books.data(), bookAmt);
+        switch (choice) {
+            case 1:
+                sortBooksByYear(books.data(), bookAmt);
+            break;
+            case 2:
+                cout << "Books sorted by publish date:" << endl;
+            for (int i = 0; i < bookAmt; i++) {
+                output(books[i].title, books[i].author, books[i].year_published);
+            }
+            break;
+            case 3:
+                cout << "Average year published of your books in your library: " << calculateAverage(books.data(), bookAmt) << endl;
+            break;
+            case 4: {
+                string input{"y"};
+                cout << "Would you like to save your library as an external file? (Default: 'y') ('n' to refuse)" << endl;
+                cin >> input;
+                if (input != "n") {
+                    cout << "Saving your library as an external file..." << endl;
+                    exportImport(books.data(), bookAmt);
+                }
+                break;
+            }
+            case 5:
+                addBook(books, bookAmt,libName);
+            break;
 
-    cout << "Books sorted by publish date:" << endl;
-    for (int i = 0; i < bookAmt; i++) {
-        output(books[i].title, books[i].author, books[i].year_published);
-    };
+            case 6: {
+                /**
+     * Catching/testing a exception code (testing code)
+     **/
+                try { // it tries to see if a exception is triggered
+                    // Assuming we have some books array and bookAmt is the number of books
+                    vector<Book> books(3);
+                    books[0] = {"Book One", "Author One", 2001};
+                    books[1] = {"Book Two", "Author Two", 2005};
+                    books[2] = {"Book Three", "Author Three", 2010};
 
+                    // Intentionally use an invalid size to test exception handling
+                    int avgYear = calculateAverage(books.data(), 0); // This should throw an exception
+                    cout << "Average year published: " << avgYear << endl;
+                } catch (const std::invalid_argument& e) { //if it is triggered, it jumps to print a error msg
+                    // Catch the exception and print an error message
+                    cout << "Error: " << e.what() << endl; //e.what() provides the error message in the throw argument
+                }
+                /**
+                * Catching/testing a exception code (testing code)
+                **/
+            }
+            break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10: {
+                cout << "Closing..." << endl;
+                cout << "Thank you for using the library system for: " << libName << " ! " << endl;
+                cout << "------------------------!TERMINATED!------------------------" << endl;
+            }
+
+                break;
+            default:
+                cout << "Invalid choice or Work in progress option" << endl;
+        }
+    }
 
 
     return 0;
